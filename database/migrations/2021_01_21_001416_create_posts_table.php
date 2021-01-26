943576c3a84a3e45efe8a58a6e2b50cfe7db6e95
Alx7-PC\Alx7 <alassiag@gmail.com>
2021-01-26 02:09:55 -0300

    first commit

---
+++ database/migrations/2021_01_21_001416_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PharIo\Manifest\Author;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('summary');
+            $table->text('image');
+            $table->text('description');
+            $table->text('author');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
